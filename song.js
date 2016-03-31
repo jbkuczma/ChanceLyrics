0da689fd8e08551fb52bcbf31d59f05f7ba85e0e
James <jbkuczma@buffalo.edu>
2016-03-31 14:10:39 -0400

    able to pick a random lyric from json file and display it on the main page

--- song.js
+++ song.js
@@ -1,4 +1,10 @@
 $.getJSON('lyrics.json',function(data){
     var albumSelected = data[Math.floor(Math.random() * data.length)];
-    console.log(albumSelected);
+    var ran = Math.floor(Math.random() * albumSelected["Songs"].length);
+    var lyric = albumSelected["Songs"][ran];
+    var album = albumSelected["Album"];
+    document.getElementById('lyric').innerHTML = lyric;
+    document.getElementById('album').innerHTML = album;
 })
+//have json file have arrays for each song, which contains the lyrics
+// for that song, for each album

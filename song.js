a4efca7ad74c99c590772ee701ca0ce69ebbe731
James <jbkuczma@buffalo.edu>
2016-04-01 08:29:50 -0400

    lyrics will load when page loads and will change whenever there is a mouse click. still need to add more lyrics and the songs that the lyric is from

--- song.js
+++ song.js
@@ -1,10 +1,22 @@
-$.getJSON('lyrics.json',function(data){
-    var albumSelected = data[Math.floor(Math.random() * data.length)];
-    var ran = Math.floor(Math.random() * albumSelected["Songs"].length);
-    var lyric = albumSelected["Songs"][ran];
-    var album = albumSelected["Album"];
-    document.getElementById('lyric').innerHTML = lyric;
-    document.getElementById('album').innerHTML = album;
+function assignInfo(){
+    $.getJSON('lyrics.json',function(data){
+        var albumSelected = data[Math.floor(Math.random() * data.length)];
+        var ran = Math.floor(Math.random() * albumSelected["Songs"].length);
+        var lyric = albumSelected["Songs"][ran];
+        var album = albumSelected["Album"];
+        document.getElementById('lyric').innerHTML = lyric;
+        document.getElementById('album').innerHTML = album;
+    })
+}
+
+$(function(){
+    assignInfo();
+    //above executes when page first loads
+    //below executes on every mouse clicks
+    $(document).click(function(){
+        assignInfo();
+    })
 })
-//have json file have arrays for each song, which contains the lyrics
+
+// change json file to have arrays for each song, which contains the lyrics
 // for that song, for each album
